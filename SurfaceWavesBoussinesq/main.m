ec6f56830afedd14bd222fb63cf2276ad936c600
JeffreyEarly <jeffrey@earlyinnovations.com>
2014-12-12 14:15:26 -0800

    Initial Commit

---
+++ SurfaceWavesBoussinesq/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SurfaceWavesBoussinesq
+//
+//  Created by Jeffrey Early on 12/12/14.
+//  Copyright (c) 2014 Early Innovations, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
